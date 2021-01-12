ea400c60314046e528b97d40b5e212bb08ac6b22
hammerhearted <nataran2001@mail.ru>
2021-01-12 22:28:46 +0500

    ,

---
+++ PhotoEnhancer/PhotoEnhancer/Program.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Windows.Forms;
+using System.Drawing;
+
+namespace PhotoEnhancer
+{
+    static class Program
+    {
+        /// <summary>
+        /// Главная точка входа для приложения.
+        /// </summary>
+        [STAThread]
+        static void Main()
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+
+            var mainForm = new MainForm();
+
+            mainForm.AddFilter(new PixelFilter<LighteningParameters>(
+                "Осветление/затемнение",
+                (pixel, parameters) => pixel * parameters.Coefficient
+                ));
+
+            mainForm.AddFilter(new PixelFilter<EmptyParameters>(
+                "Оттенки серого",
+                (pixel, parameters) =>
+                {
+                    var chanel = 0.3 * pixel.R +
+                                0.6 * pixel.G +
+                                0.1 * pixel.B;
+
+                    return new Pixel(chanel, chanel, chanel);
+                }
+                ));
+
+            mainForm.AddFilter(new TransformFilter(
+                "Отражение по горизонтали",
+                size => size,
+                (point, size) => new Point(size.Width - point.X - 1, point.Y)
+                ));
+
+            mainForm.AddFilter(new TransformFilter(
+                "Поворот на 90° против ч. с.",
+                size => new Size(size.Height, size.Width),
+                (point, size) => new Point(size.Width - point.Y - 1, point.X)
+                ));
+
+
+            mainForm.AddFilter(new TransformFilter<RotationParameters>(
+                "Свободное вращение", new RotateTransformer()));
+
+            Application.Run(mainForm);
+        }
+    }
+}
